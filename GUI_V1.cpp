a48aab4d5036a2dbf8983bc11acdc7097d688ee4
yahya chami <123872563+dzap00@users.noreply.github.com>
2023-07-18 16:23:28 +0100

    Add files via upload

---
+++ GUI_V1.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+#include "MyForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	GUIV1::MyForm form;
+	Application::Run(% form);
+}
